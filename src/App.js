ac764b86c3e1762057cd4e5ab0c8b78266593c41
tuongnc <s3818196@rmit.edu.vn>
2021-06-12 21:36:49 +0700

    done-to-do-list

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React, {Component} from 'react';
 import './App.css';
 import './components/TodoItem'
 import TodoItem from './components/TodoItem';
-import TrafficLight from './components/TrafficLight';
-
+import classNames from 'classnames';
+// import TrafficLight from './components/TrafficLight';
+import img3 from './img/check_1.svg'
 
 
 
@@ -11,9 +12,11 @@ class App extends Component {
   constructor(){
     super()
     this.state = {
+      currentFilter : "ALL",
+      newItem :'',
       TodoItems : [
         {title: "Item 1", isComplete: false},
-        {title: "Item 2123",isComplete: false},
+        {title: "Item 2123",isComplete: true},
         {title: "Item 3", isComplete: false}
        ]
     }
@@ -29,6 +32,7 @@ class App extends Component {
     const isComplete= item.isComplete;
     this.setState(
       {
+        newItem:'',
         TodoItems :[
           ...TodoItems.slice(0,index),
           {
@@ -39,21 +43,141 @@ class App extends Component {
         ]
       }
     ) 
-    }
-     
+    }    
 
   }
 
+  onKeyUp(event){
+    if(event.keyCode === 13) {
+
+      let txt = event.target.value
+      txt = txt.trim();
+      if (!txt) {
+        return;
+      }
+      
+
+      this.setState(
+        {
+          newItem:'',
+          TodoItems: [
+            { 
+              title: txt ,
+              isComplete: false
+            },
+            ...this.state.TodoItems
+          ]
+        }
+      );
+    }
+  }
+
+  onChange(event) {
+    this.setState(
+      {
+        newItem: event.target.value
+      }
+    )
+  }
+ 
+  onClickAll = () => {
+    let isComplete = !this.state.TodoItems[0].isComplete;
+    let newArray = this.state.TodoItems;
+
+    newArray.forEach(item => {
+      item.isComplete = isComplete;
+    });
+
+    this.setState(
+      {
+        TodoItems : newArray
+      }
+    )
+
+  }
+
+
+  
+
+  clickAll = () => {
+    this.setState(
+      {
+        currentFilter : "ALL"
+      }
+    )
+  }
+
+  clickActive = () => {
+    console.log("ACTIVE")
+    this.setState(
+      {
+        currentFilter : "ACTIVE"
+      }
+    )
+  }
+
+  clickCOMPLETED = () => {
+    this.setState(
+      {
+        currentFilter : "COMPLETED"
+      }
+    )
+  }
+
+  onDeleteOne(item, index){
+    let updateArr = [...this.state.TodoItems.slice(0,index),...this.state.TodoItems.slice(index+1)];
+  
+
+    this.setState({
+      TodoItems : updateArr
+    });
+  }
+
+  clearAll= () =>{
+    this.setState({
+      TodoItems : []
+    });
+  }
+
   render(){
+    var count = 0;
+    this.state.TodoItems.forEach(element => {
+      if (element.isComplete === false) {
+        count++;
+      }
+    });
+   
+
     return (
       <div className="App">
-        {
+        <div className="Header">
+          <img src={img3} height={32} width={32} onClick={() => this.onClickAll()} />
+          <input 
+            placeholder="Enter a task" 
+            onKeyUp={(event)=> this.onKeyUp(event)}
+            value={this.state.newItem}
+            onChange={(event) => this.onChange(event)}
+          ></input>
+        </div>
         
-        this.state.TodoItems.length > 0 ? this.state.TodoItems.map((item, index) => <TodoItem key={index} item={item} onclickFunction={this.onClickItem(item,index)} />)  : 'Nothing item'  
-  
+      
+        {        
+        this.state.TodoItems.length > 0 ? this.state.TodoItems.map((item, index) => <TodoItem key={index} item={item} onclickFunction={this.onClickItem(item,index)} display={this.state.currentFilter} onDelete={() => this.onDeleteOne(item,index)} />)  : 'Nothing item'  
         }
   
-        
+        <div className="Footer">
+           <div className="totalActive">
+             {count} items left
+           </div>
+           <div className="filter">
+              <div className={classNames({'selected': (this.state.currentFilter === 'ALL')} )} onClick={()=>this.clickAll()}>ALL</div>
+              <div className={classNames({'selected': (this.state.currentFilter === 'ACTIVE')} )} onClick={()=>this.clickActive()}>Active</div>
+              <div className={classNames({'selected': (this.state.currentFilter === 'COMPLETED')} )} onClick={() => this.clickCOMPLETED()}>Completed</div>
+           </div>
+           <div className="clear" onClick={()=> this.clearAll()}>
+              Clear all
+           </div>
+        </div>
     
       </div>
     );
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,11 +2,12 @@ import React, {Component} from 'react';
 import './TodoItem.css';
 import classNames from 'classnames';
 import checkImg from '../img/check.svg';
-import checkImg1 from '../img/tick.png'
+import checkImg1 from '../img/tick.png';
+import cancelImg from '../img/cancel.svg'
 class TodoItem extends Component {
     
     render() {
-        const {item , onclickFunction } = this.props;
+        const {item , onclickFunction, display, onDelete } = this.props;
         // let className = "TodoItem";
         // if (item.isComplete){
         //     className += " TodoItem-complete";
@@ -15,11 +16,19 @@ class TodoItem extends Component {
         if (item.isComplete){
             url =checkImg1;
         } 
+        
+        if (display === 'ACTIVE') {
+            var flagActive = true;
+        }
+        if (display === 'COMPLETED') {
+            var flagCom = true;
+        }
         return (
-            <div  className={classNames('TodoItem',{'TodoItem-complete' : item.isComplete})}>
+            <div  className={classNames('TodoItem',{'TodoItem-complete' : item.isComplete}, {'notShow' : (item.isComplete) && (flagActive)}, {'notShow' : (!item.isComplete) && (flagCom)} )}>
                 <img onClick={onclickFunction} src={url} width={32} height={32} />
-                <p> Hellooooo {item.title}</p>
+                <p>  {item.title}</p>
                  {/* <img style={{ width: 50, height: 50 }} src="img1.jpg"/>  */}
+                 <img onClick={onDelete} src={cancelImg} width={32} height={32}  className="cancelBtn"/>
             </div>
             
         );
